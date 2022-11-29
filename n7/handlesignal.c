a1323d678087b4e69f55628804149f065d24e32e
hj592 <ghn07096@naver.com>
2022-11-29 20:56:14 +0900

    7.시그널 관련 함수

---
+++ n7/handlesignal.c
@@ -0,0 +1,28 @@
+/* 
+handlesignal.c
+시그널 처리함수 사용
+*/
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int signo);
+int main()
+{
+	struct sigaction act;
+	int i = 0;
+	act.sa_handler = handler;
+	sigfillset(&(act.sa_mask));
+	sigaction(SIGINT, &act, NULL);
+	printf("SIGINT on\n");
+	while(1) {
+		sleep(1);
+		printf("sleep for %d sec(s).\n", ++i);
+	}
+}
+void handler(int signo)
+{	
+	printf("handler: signo=%d\n", signo);
+}
+
